4c8553f7d6f5fdfef9e7061ca245be4ac01b4830
MD-Saadman-Fuad <md.saadman.fuad@gmail.com>
2025-11-12 15:19:28 +0600

    MongoDB setup Complete

---
+++ index.js
@@ -0,0 +1,62 @@
+const express = require('express');
+const cors = require('cors');
+require('dotenv').config();
+const app = express();
+const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
+const port = process.env.PORT || 3000;
+
+
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASSWORD}@civixdb.rqfqgav.mongodb.net/?appName=civixDB`;
+const client = new MongoClient(uri, {
+    serverApi: {
+        version: ServerApiVersion.v1,
+        strict: true,
+        deprecationErrors: true,
+    }
+});
+
+app.get('/', (req, res) => {
+    res.send('CIVIX Server is running');
+});
+
+
+
+
+
+
+
+async function run() {
+    try {
+        // Connect the client to the server	(optional starting in v4.7)
+        await client.connect();
+
+        //database setup
+        const db = client.db('civixDB');
+        const issuesCollection = db.collection('issues');
+        const contributionCollection = db.collection('contributions');
+        // const usersCollection = db.collection('users');
+
+        
+
+        // Send a ping to confirm a successful connection
+        await client.db("admin").command({ ping: 1 });
+        console.log("Pinged your deployment. You successfully connected to MongoDB!");
+    } finally {
+        // Ensures that the client will close when you finish/error
+        // Do NOT close the client here — we want the server to keep the DB connection
+        // open while the process is running so route handlers can use `productsCollection`.
+        // If you need to close on shutdown, handle it in a SIGINT/SIGTERM listener.
+        // await client.close();
+    }
+}
+run().catch(console.dir);
+
+
+
+
+
+
+
+app.listen(port, () => {
+    console.log(`CIVIX Server is running on port: ${port}`);
+});
